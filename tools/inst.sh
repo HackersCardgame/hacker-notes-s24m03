9435ed9edb7ce0ead63aa120e4ddb4e79ea8183d
Your Name <you@example.com>
2024-03-18 06:41:15 +0100

    ...

--- tools/inst.sh
+++ tools/inst.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+echo "
+apt-get install tesseract-ocr-eng tesseract-ocr-deu
+#deb cdrom:[Debian GNU/Linux 12.5.0 _Bookworm_ - Official amd64 DVD Binary-1 with firmware 20240210-11:28]/ bookworm contrib main non-free-firmware
+
+deb https://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+
+deb https://deb.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+
+deb https://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+"
+
 apt-get update
 apt-get upgrade
+apt-get install screen
 apt-get purge firefox-esr
+apt-get install epiphany-browser
 apt-get install git vim sudo gnome-boxes figlet inkscape gedit mixxx 
 apt-get install libvirt-daemon-system 
 apt-get install ffmpeg
 apt-get install kdenlive
 apt-get install obs-studio
 apt-get install tesseract
+apt-get install nvidia-detect
 
-apt-get install tesseract-ocr-eng tesseract-ocr-deu
+apt-get install cups-client
+
+echo "printer installieren"
+
