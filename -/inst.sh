35abe61105c90d8ffa0f60d816c85bc923d4bc93
Your Name <you@example.com>
2024-03-18 03:13:35 +0100

    ...

---
+++ -/inst.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get purge firefox-esr
+apt-get install git vim sudo gnome-boxes figlet inkscape gedit mixxx 
+apt-get install libvirt-daemon-system 
+apt-get install ffmpeg
+apt-get install kdenlive
+apt-get install obs-studio
+apt-get install tesseract
+
+apt-get install tesseract-ocr-eng tesseract-ocr-deu
