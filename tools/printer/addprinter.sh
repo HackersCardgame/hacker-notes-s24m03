c74d846d6f4feabd8a8aa7eddfd2fa9ffdbc535d
Your Name <you@example.com>
2024-04-07 08:09:42 +0200

    ...

--- tools/printer/addprinter.sh
+++ tools/printer/addprinter.sh
@@ -4,6 +4,6 @@
 #lpadmin -p duplex -v socket://192.168.1.242 -E -m driverless
 #
 cp Brother-MFC-9450CDN-Postscript.ppd /home/marc/
-lpadmin -p duplexer -v socket://192.168.1.242 -E -i /home/marc/Brother-MFC-9450CDN-Postscript.ppd
-lpadmin -p duplexer_new -v socket://192.168.1.243 -E -i /home/marc/Brother-MFC-9450CDN-Postscript.ppd
+/usr/sbin/lpadmin -p duplexer -v socket://192.168.1.242 -E -i /home/marc/Brother-MFC-9450CDN-Postscript.ppd
+/usr/sbin/lpadmin -p duplexer_new -v socket://192.168.1.243 -E -i /home/marc/Brother-MFC-9450CDN-Postscript.ppd
 
